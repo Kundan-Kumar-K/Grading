1e5e5171e002f7569ce65975bd6f0a74c97fd789
Kundan-Kumar-K <66720068+Kundan-Kumar-K@users.noreply.github.com>
2020-06-11 08:47:13 +0530

    grading using ternary operator

--- grading.c
+++ grading.c
@@ -2,9 +2,9 @@
 main()
 {
 	int m;
-	char X;
+	char G;
 	printf("Enter your Marks : ");
 	scanf("%d", &m);
-	X = m >= 85 ? 'A' : (m >= 70 && m < 85 ? 'B' : (m >= 55 && m < 70 ? 'C' : (m >= 40 && m < 55 ? 'D' : 'F')));
-	printf("Your Grade is %c", X);
+	G = m >= 85 ? 'A' : (m >= 70 && m < 85 ? 'B' : (m >= 55 && m < 70 ? 'C' : (m >= 40 && m < 55 ? 'D' : 'F')));
+	printf("Your Grade is %c", G);
 }
