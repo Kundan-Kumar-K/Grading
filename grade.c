817fd88e736df75fae6fa317e273da9d685921e3
Kundan-Kumar-K <66720068+Kundan-Kumar-K@users.noreply.github.com>
2020-06-11 09:02:24 +0530

    grading with nested esle if

---
+++ grade.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+main()
+{
+	int m;
+	char G;
+	printf("Enter your Marks : ");
+	scanf("%d", &m);
+	if (m >= 85)
+	{
+		G = 'A';
+	}
+	else if (m >= 70 && m < 85)
+	{
+		G = 'B';
+	}
+	else if (m >= 55 && m < 70)
+	{
+		G = 'C';
+	}
+	else if (m >= 40 && m < 55)
+	{
+		G = 'D';
+	}
+	else
+	{
+		G = 'F';
+	}
+	printf("Your Grade is %c", G);
+}
