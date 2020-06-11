4dbb6325e09f454e8a2fa26b717a8345ae5b10c8
Kundan-Kumar-K <66720068+Kundan-Kumar-K@users.noreply.github.com>
2020-06-11 08:46:14 +0530

    Update grading.c

--- grading.c
+++ grading.c
@@ -4,7 +4,7 @@ main()
 	int m;
 	char X;
 	printf("Enter your Marks : ");
-	scanf_s("%d", &m);
+	scanf("%d", &m);
 	X = m >= 85 ? 'A' : (m >= 70 && m < 85 ? 'B' : (m >= 55 && m < 70 ? 'C' : (m >= 40 && m < 55 ? 'D' : 'F')));
 	printf("Your Grade is %c", X);
 }
